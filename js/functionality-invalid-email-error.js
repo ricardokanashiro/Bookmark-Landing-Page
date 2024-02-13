b75d0fb9daa5b832b2cf720197fe5a730fb373b8
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-02-13 14:59:12 -0300

    adding error message in join email input

--- js/functionality-invalid-email-error.js
+++ js/functionality-invalid-email-error.js
@@ -1,11 +1,13 @@
 const emailInput = document.querySelector('.join__contact-area input');
 const errorIcon = document.querySelector('.contact-area__input-wrapper img');
+const errorMessage = document.querySelector('.contact-area__input-wrapper i');
 
 function triggerError(event) {
     event.preventDefault();
 
     errorIcon.style.display = 'block';
     event.target.style.borderColor = 'hsl(0, 94%, 66%)';
+    errorMessage.style.visibility = 'visible';
 }
 
 emailInput.addEventListener('invalid', (event) => triggerError(event));
